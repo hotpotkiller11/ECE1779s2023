6dc4d7fa9c0f343b9a5661f19af5e8773bb4f3fa
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-02-15 12:01:07 -0500

    feat. get, post, clear

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from Controller.CacheController import CacheController
 
 webapp = Flask(__name__)
+memcache_list = ["127.0.0.1:7000", "127.0.0.1:7001"]
 
 from Controller import main
+
+controller = CacheController(memcache_list)
--- Controller/main.py
+++ Controller/main.py
@@ -1,84 +1,33 @@
 
 from flask import  request, g
 import datetime
-from Controller import webapp
+from Controller import webapp, controller
 import random
 import mysql.connector
 from Controller.config import db_config
 from apscheduler.schedulers.background import BackgroundScheduler
+from Controller.CacheController import CacheController
 import atexit
 import json
-import hashlib
+import requests
 
-# Functions
 
-def get_hash(key: str) -> bytes:
-    return hashlib.md5(str.encode('utf-8')).hexdigest()
-
-class CacheController:
-    """ CacheController class provide an object that manage all 
-        memcache nodes as a whole. Provides method to communicate 
-        with certain memcache node or broadcast configuration
-        modification. 
-        
-        Fixed 16 partitions (16 nodes)
-    """
-
-    memcache_nodes = []
-    pool_size = 0
-    partition_dict = {} # used to map partition with memcache nodes
-
-    def __init__(self, memcache_servers: list[str]):
-        """ Create a new Cache Controller instance with a list of memcache node
-            servers.
-
-        Args:
-            memcache_servers (list[str]): list of addresses of the memcache node
-        """
-        assert(len(memcache_servers) > 0)
-        self.memcache_nodes = memcache_servers
-        self.pool_size = 1
-        # 16 partitions all map to the same node
-        for i in range(16):
-            self.partition_dict[hex(i)[2:]] = memcache_servers[0]
-
-    def get_node(self, key: str) -> str:
-        """ Get corresponding node address by key.
-
-        Args:
-            key (str): key
-
-        Returns:
-            str: the address of the corresponding node
-        """
-        h = get_hash(key)
-        node = self.partition_dict[h[0]]
-        return node
+@webapp.route('/put', methods=['POST', 'GET'])
+def PUT():
+    key = request.json["key"]
+    node = controller.get_node(key)
+    requests.post(node + "/put", json = json.dumps(request.json))
     
-    def modify_pool_size(self, size: int):
-        """ Modify the number of activating node, and convey data stored in 
-            old nodes into newer ones.
 
-        Args:
-            size (int): new size of the activated nodes
+@webapp.route('/get', methods=['POST', 'GET'])
+def GET():
+    key = request.json["key"]
+    node = controller.get_node(key)
+    requests.post(node + "/get", json = json.dumps(request.json))
 
-        Raises:
-            ValueError: size not 
-        """
-        if size <= 0: raise ValueError("Size must greater than 0")
-        if size > size: raise ValueError("Size must smaller or equal to pool size (%d)" % (self.pool_size))
-        self.pool_size = size
-        new_partition_dict = {}
-        # generate new partition dict
-        for i in range(16):
-            new_partition_dict[hex(i)[2:]] = self.memcache_nodes[i % size]
-    
-        for i in range(16):
-            if new_partition_dict[hex(i)[2:]] != self.partition_dict[hex(i)[2:]]:
-                # Notify node to convey data to new place
-                pass
-        
-        # Update the partition dict
-        self.partition_dict = new_partition_dict
-
-    
+@webapp.route('/clear',methods=['POST', 'GET'])
+def CLEAR():
+    # clear all activated nodes
+    nodes = controller.activated_nodes()
+    for node in nodes:
+        requests.post(node + "/clear")
