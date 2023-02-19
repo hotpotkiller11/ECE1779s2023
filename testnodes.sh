2a807847cc194026daf0d1fe21083880b60ef0df
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-02-19 11:12:19 -0500

    test. linux shell

--- testnodes.sh
+++ testnodes.sh
@@ -1,2 +1,7 @@
 python3 ./run_node.py 7000 &
 python3 ./run_node.py 7001 &
+# Modification according to advice of chatGPT
+echo ""
+echo "Press any key to stop all instances."
+read -n 1 -s
+pkill -f "python ./run_nodes.py"
