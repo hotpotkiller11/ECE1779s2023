11889b262ac923f7e2cedbd80360e1ac47cc4203
hotpotkiller11 <zzq2018@bupt.edu.cn>
2023-03-04 02:56:01 -0500

    securitykey

---
+++ toolAWS/securitykey.py
@@ -0,0 +1,128 @@
+import tempfile
+import boto3
+import os
+
+from botocore.args import logger
+from botocore.exceptions import ClientError
+
+
+class KeyPairWrapper:
+    """Encapsulates Amazon Elastic Compute Cloud (Amazon EC2) key pair actions."""
+    def __init__(self, ec2_resource, key_file_dir, key_pair=None):
+        """
+        :param ec2_resource: A Boto3 Amazon EC2 resource. This high-level resource
+                             is used to create additional high-level objects
+                             that wrap low-level Amazon EC2 service actions.
+        :param key_file_dir: The folder where the private key information is stored.
+                             This should be a secure folder.
+        :param key_pair: A Boto3 KeyPair object. This is a high-level object that
+                         wraps key pair actions.
+        """
+        self.ec2_resource = ec2_resource
+        self.key_pair = key_pair
+        self.key_file_path = None
+        self.key_file_dir = key_file_dir
+
+    @classmethod
+    def from_resource(cls):
+        ec2_resource = boto3.resource('ec2')
+        return cls(ec2_resource, tempfile.TemporaryDirectory())
+
+    def create(self, key_name):
+        """
+        Creates a key pair that can be used to securely connect to an EC2 instance.
+        The returned key pair contains private key information that cannot be retrieved
+        again. The private key data is stored as a .pem file.
+        :param key_name: The name of the key pair to create.
+        :return: A Boto3 KeyPair object that represents the newly created key pair.
+        """
+        try:
+            self.key_pair = self.ec2_resource.create_key_pair(KeyName=key_name)
+            self.key_file_path = os.path.join(self.key_file_dir.name, f'{self.key_pair.name}.pem')
+            with open(self.key_file_path, 'w') as key_file:
+                key_file.write(self.key_pair.key_material)
+        except ClientError as err:
+            logger.error(
+                "Couldn't create key %s. Because: %s: %s", key_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return self.key_pair
+
+    def delete(self):
+        """
+        Deletes a key pair.
+        """
+        if self.key_pair is None:
+            logger.info("No key pair to delete.")
+            return
+
+        key_name = self.key_pair.name
+        try:
+            self.key_pair.delete()
+            self.key_pair = None
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete key %s. Here's why: %s : %s", key_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+class ElasticIpWrapper:
+    """Encapsulates Amazon Elastic Compute Cloud (Amazon EC2) Elastic IP address actions."""
+    def __init__(self, ec2_resource, elastic_ip=None):
+        """
+        :param ec2_resource: A Boto3 Amazon EC2 resource. This high-level resource
+                             is used to create additional high-level objects
+                             that wrap low-level Amazon EC2 service actions.
+        :param elastic_ip: A Boto3 VpcAddress object. This is a high-level object that
+                           wraps Elastic IP actions.
+        """
+        self.ec2_resource = ec2_resource
+        self.elastic_ip = elastic_ip
+
+    @classmethod
+    def from_resource(cls):
+        ec2_resource = boto3.resource('ec2')
+        return cls(ec2_resource)
+
+    def allocate(self):
+        """
+        Allocates an Elastic IP address that can be associated with an Amazon EC2
+        instance. By using an Elastic IP address, you can keep the public IP address
+        constant even when you restart the associated instance.
+        :return: The newly created Elastic IP object. By default, the address is not
+                 associated with any instance.
+        """
+        try:
+            response = self.ec2_resource.meta.client.allocate_address(Domain='vpc')
+            self.elastic_ip = self.ec2_resource.VpcAddress(response['AllocationId'])
+        except ClientError as err:
+            logger.error(
+                "Couldn't allocate Elastic IP. Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return self.elastic_ip
+
+    def associate(self, instance):
+        """
+        Associates an Elastic IP address with an instance. When this association is
+        created, the Elastic IP's public IP address is immediately used as the public
+        IP address of the associated instance.
+        :param instance: A Boto3 Instance object. This is a high-level object that wraps
+                         Amazon EC2 instance actions.
+        :return: A response that contains the ID of the association.
+        """
+        if self.elastic_ip is None:
+            logger.info("No Elastic IP to associate.")
+            return
+
+        try:
+            response = self.elastic_ip.associate(InstanceId=instance.id)
+        except ClientError as err:
+            logger.error(
+                "Couldn't associate Elastic IP %s with instance %s. Here's why: %s: %s",
+                self.elastic_ip.allocation_id, instance.id,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        return response
