d1bc500a62fa73eaadbd3aa8974dba032c96c44e
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-04 11:48:55 -0500

    delete. memcache sql

--- MemCache/__init__.py
+++ MemCache/__init__.py
@@ -13,31 +13,31 @@ main.reqs = 0
 
 # Fetch memcache config upon initialization, default value: 4KB, random replacement
 
-cnx = main.init_db()
-query = '''SELECT capacity, policy
-                FROM backend_config where id = (
-    select max(id) FROM backend_config);'''
-cursor = cnx.cursor()
-cursor.execute(query)
-rows = cursor.fetchall()
-if len(rows) > 0:
-    main.Config = {'capacity': rows[0][0], 'policy': rows[0][1]}
-else:
-    query = '''INSERT INTO `backend_config` (`capacity`, `policy`) VALUES (1024000, "random")'''
-    cursor = cnx.cursor()
-    cursor.execute(query)
-    cnx.commit() # Try to commit (confirm) the insertion
-    cursor.close()
-    main.Config = {'capacity': 1024000, 'policy': "random"} # Default value: 1000KB
+# cnx = main.init_db()
+# query = '''SELECT capacity, policy
+#                 FROM backend_config where id = (
+#     select max(id) FROM backend_config);'''
+# cursor = cnx.cursor()
+# cursor.execute(query)
+# rows = cursor.fetchall()
+# if len(rows) > 0:
+#     main.Config = {'capacity': rows[0][0], 'policy': rows[0][1]}
+# else:
+#     query = '''INSERT INTO `backend_config` (`capacity`, `policy`) VALUES (1024000, "random")'''
+#     cursor = cnx.cursor()
+#     cursor.execute(query)
+#     cnx.commit() # Try to commit (confirm) the insertion
+#     cursor.close()
+#     main.Config = {'capacity': 1024000, 'policy': "random"} # Default value: 1000KB
 
-# Clear stat from last run
-query = '''DELETE FROM `backend_statistic`'''
-cursor = cnx.cursor()
-cursor.execute(query)
-cnx.commit()
-cursor.close()
-cnx.close()
+# # Clear stat from last run
+# query = '''DELETE FROM `backend_statistic`'''
+# cursor = cnx.cursor()
+# cursor.execute(query)
+# cnx.commit()
+# cursor.close()
+# cnx.close()
 
-cnx.close()
+# cnx.close()
 
 # main.Config = {'capacity': 400, 'policy': "random"}
--- MemCache/main.py
+++ MemCache/main.py
@@ -29,34 +29,34 @@ stat_list = [] # (hit, miss) queue, update every 5 seconds
 mem_dict = {}
 key_queue = [] # LRU list, from the most recent use to the least recent use
 
-"""DB config """
-def init_db():
-    """
-    initialization database
-    :return: database initialization
-    """
+# """DB config """
+# def init_db():
+#     """
+#     initialization database
+#     :return: database initialization
+#     """
 
-    return mysql.connector.connect(user=db_config['user'],
-                                   password=db_config['password'],
-                                   host=db_config['host'],
-                                   database=db_config['database'])
+#     return mysql.connector.connect(user=db_config['user'],
+#                                    password=db_config['password'],
+#                                    host=db_config['host'],
+#                                    database=db_config['database'])
 
-def get_db():
-    """
-    init step 2
-    :return: database
-    """
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = init_db()
-    return db
+# def get_db():
+#     """
+#     init step 2
+#     :return: database
+#     """
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = init_db()
+#     return db
 
-@webapp.teardown_appcontext
-def teardown_db(exception):
-    """Safe tear down"""
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+# @webapp.teardown_appcontext
+# def teardown_db(exception):
+#     """Safe tear down"""
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
 
 
 def get_config_info():
