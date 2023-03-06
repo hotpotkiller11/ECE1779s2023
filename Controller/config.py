eb9529dfa60f6027852b029981b846804b70e506
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-06 10:40:56 -0500

    fix. database endpoint to host

--- Controller/config.py
+++ Controller/config.py
@@ -5,7 +5,7 @@
 
 db_config = {'user': 'admin', #ur db
              'password': '12345678',
-             'Endpoint': 'ece1779sqldb.cr3xgzhrvhr7.us-east-1.rds.amazonaws.com',
+             'host': 'ece1779sqldb.cr3xgzhrvhr7.us-east-1.rds.amazonaws.com',
              'database': 'ece1779'}
 
 IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
