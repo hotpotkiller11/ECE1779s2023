affa5edd9c431aa40c689b57c20a6c50606692fd
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-06 10:39:21 -0500

    update rds databases

--- MemCache/config.py
+++ MemCache/config.py
@@ -1,12 +1,12 @@
-db_config = {'user': 'root',
-             'password': '123456',
-             'host': '127.0.0.1',
-             'database': 'ece1779'}
+# db_config = {'user': 'root',
+#              'password': '123456',
+#              'host': '127.0.0.1',
+#              'database': 'ece1779'}
 
-"""db_config = {'user': 'admin', #ur db
+db_config = {'user': 'admin', #ur db
              'password': '12345678',
              'Endpoint': 'ece1779sqldb.cr3xgzhrvhr7.us-east-1.rds.amazonaws.com',
-             'database': 'ece1779'}"""
+             'database': 'ece1779'}
 
 IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
                 '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.xcf', '.ai', 'cdr']
