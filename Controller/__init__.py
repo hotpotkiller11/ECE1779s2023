518e018e50baaa0453cc2c52fb0ae8dbbd68afe9
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-06 16:19:31 -0500

    update. second node

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from Controller.CacheController import CacheController
 
 webapp = Flask(__name__)
-memcache_list = ["http://172.31.91.73:5000"]
+memcache_list = ["http://172.31.91.73:5000", "http://172.31.59.184:5000"]
 control = CacheController(memcache_list)
 # control.modify_pool_size(1)
 
