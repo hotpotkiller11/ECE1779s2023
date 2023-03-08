fa56f023e492fa7d76cb4bede7de1fb22270c283
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-08 10:53:56 -0500

    feat. read all stats

--- FrontEnd/stat.py
+++ FrontEnd/stat.py
@@ -3,58 +3,54 @@ from FrontEnd.db_connect import get_db
 from FrontEnd import webapp
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import render_template
-import atexit
+from toolAWS import cloudWatch
+import boto3
+
+cloudwatch = boto3.client('cloudwatch')
+ec2 = boto3.resource('ec2')
+statManager = cloudWatch.CloudWatchWrapper(cloudwatch)
 
 def get_stats() :#-> list[dict]:
     """
-    get currenr stats
+    get current stats
     :return: list[dict]
     """
-    result = []
-
-    now = datetime.now()
-    boundary = now - timedelta(minutes = 10) # Get data of last 10 minutes
-    boundary = boundary.strftime('%Y-%m-%d %H:%M:%S')
-
-    db = get_db()
-
-    cursor = db.cursor()
-    query = "SELECT `hit`, `miss`, `request_count`, `size`, `picture_count` FROM `backend_statistic` WHERE `timestamp` > '%s' " % boundary # ordered by timestamp by default
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    cursor.close()
-
-    for r in rows:
-        point = {}
-        point["hit_rate"] = r[0] / max(r[0] + r[1], 1)
-        point["miss_rate"] = r[1] / max(r[0] + r[1], 1)
-        point["size"] = r[3]
-        point["count"] = r[4]
-        point["req"] = r[2]
-        result.append(point)
+    # 3 increment values
+    miss = statManager.monitor_stat("miss", "Sum")
+    hit = statManager.monitor_stat("hit", "Sum")
+    req = statManager.monitor_stat("req", "Sum")
+    # 2 scaler values
+    size = statManager.monitor_stat("size", "Average")
+    items = statManager.monitor_stat("numitem", "Average")
+    result = {"miss":  miss,
+              "hit":   hit,
+              "req":   req,
+              "size":  size,
+              "items": items}
     return result
 
 @webapp.route('/statistic', methods = ['GET','POST'])
-
 def stat():
     """
     build the statistic list
     :return: html page render
     """
-    hit_xy = []
-    miss_xy = []
-    size_xy = []
-    count_xy = []
-    req_xy = []
-    stat_list = get_stats()
-    length = len(stat_list)
-    time = (length - 1) * -5
-    for p in stat_list:
-        hit_xy.append({"x": time, "y": p["hit_rate"]})
-        miss_xy.append({"x": time, "y": p["miss_rate"]})
-        size_xy.append({"x": time, "y": p["size"]})
-        count_xy.append({"x": time, "y": p["count"]})
-        req_xy.append({"x": time, "y": p["req"]})
-        time += 5
+    # hit_xy = []
+    # miss_xy = []
+    # size_xy = []
+    # count_xy = []
+    # req_xy = []
+    # stat_list = get_stats()
+    # length = len(stat_list)
+    # time = (length - 1) * -5
+    # for p in stat_list:
+    #     hit_xy.append({"x": time, "y": p["hit_rate"]})
+    #     miss_xy.append({"x": time, "y": p["miss_rate"]})
+    #     size_xy.append({"x": time, "y": p["size"]})
+    #     count_xy.append({"x": time, "y": p["count"]})
+    #     req_xy.append({"x": time, "y": p["req"]})
+    #     time += 5
 
-    return render_template("statistic.html", hit_xy = hit_xy, miss_xy = miss_xy, size_xy = size_xy, count_xy = count_xy, req_xy = req_xy)
+    # return render_template("statistic.html", hit_xy = hit_xy, miss_xy = miss_xy, size_xy = size_xy, count_xy = count_xy, req_xy = req_xy)
+    
+    return get_stats()
