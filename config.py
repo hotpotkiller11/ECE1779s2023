509f6e8678d2a7019dd9e0f61d624bbf8d63b1af
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-11 14:13:16 -0500

    update. full nodes on

--- config.py
+++ config.py
@@ -11,4 +11,7 @@ db_config = {'user': 'admin', #ur db
 IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
                 '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.xcf', '.ai', 'cdr']
 
-memcache_id_list = ['i-0e77ca1f0c30ae1fc']
+memcache_id_list = ['i-0e77ca1f0c30ae1fc', "i-0383882259b6170ef", 
+                    "i-0bd4b20dcd8282d30", "i-0fb6b3a88ff95d765", 
+                    "i-077424fc0449b8e72", "i-045804fa47a88a7ce", 
+                    "i-026d9de69d1dfeeeb", "i-035901a2e5ea503ea"]
