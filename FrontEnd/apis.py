907392d84e1339f0aae5f57337d4851e89f27739
jymikezhang <jymike.zhang@mail.utoronto.ca>
2023-03-10 20:03:41 -0500

    fix(API-configure cache)

--- FrontEnd/apis.py
+++ FrontEnd/apis.py
@@ -251,56 +251,62 @@ def getRate(parameters):
 
 @webapp.route('/api/configure_cache/',methods=['POST'])
 def configure_cache():
-    #res=request.form
-    # if 'mode' in res:
-    mode = request.args.get("mode")
-    print(mode)
-    print(type(mode))
-    # if 'numNodes' in res:
-    numNodes=request.args.get("numNodes")
-    print(numNodes)
-    print(type(numNodes))
-    res = requests.post(backend + '/pool', json = {"new_active": int(numNodes)})
-    # if 'cacheSize' in res:
-    cacheSize = request.args.get("cacheSize")
-    print(cacheSize)
-    print(type(cacheSize))
-    # if 'policy' in res:
-    policy  = request.args.get("policy")
-    # try:
-    capacity=float(cacheSize)
-    capacity *= 1024 * 1024
-    print(capacity)
-    print(type(capacity))
-    save_conf_todb(capacity,policy)
-    # except Exception as e:
-        # data = {
-        #     "success" : "false"
-        # }
-        # response = webapp.response_class(
-        #     response=json.dumps(data),
-        #     status=500,
-        #     mimetype='application/json'
-        # )
-        # return response
-    # if 'expRatio' in res:
-    expRatio= request.args.get("expRatio")
-    # if 'shrinkRatio' i res:
-    shrinkRatio = request.args.get("shrinkRatio")
-    # if 'maxMiss' in res:
-    maxMiss= request.args.get("maxMiss")
-    # if 'minMiss' in res:
-    minMiss= request.args.get("minMiss")
-    data = {
-                    "success": "true",
-                    "mode": [mode],
-                    "numNodes": [numNodes],
-                    "cacheSize": [int(capacity/(1024*1024))],
-                    "policy": [policy]
-                }
-    response = webapp.response_class(
-                    response=json.dumps(data),
-                    status=500,
-                    mimetype='application/json'
-                )
-    return response
+    try:
+        # if 'mode' in res:
+        mode = request.args.get("mode")
+        print(type(mode))
+        print(type(str(mode)))
+        if(str(mode) is 'manual'):
+            requests.post(backend + '/auto',json={"auto":False})
+        elif(str(mode) is 'auto'):
+            requests.post(backend + '/auto',json={"auto":True})
+        # if 'numNodes' in res:
+        numNodes=request.args.get("numNodes")
+        # print(numNodes)
+        # print(type(numNodes))
+        res = requests.post(backend + '/pool', json = {"new_active": int(numNodes)})
+        # if 'cacheSize' in res:
+        cacheSize = request.args.get("cacheSize")
+        # print(cacheSize)
+        # print(type(cacheSize))
+        # if 'policy' in res:
+        policy  = request.args.get("policy")
+        # try:
+        capacity=float(cacheSize)
+        capacity *= 1024 * 1024
+        # print(capacity)
+        # print(type(capacity))
+        save_conf_todb(capacity,policy)
+        # if 'expRatio' in res:
+        expRatio= request.args.get("expRatio")
+        # if 'shrinkRatio' i res:
+        shrinkRatio = request.args.get("shrinkRatio")
+        # if 'maxMiss' in res:
+        maxMiss= request.args.get("maxMiss")
+        # if 'minMiss' in res:
+        minMiss= request.args.get("minMiss")
+        requests.post(backend + '/auto_params',json = {"max_miss":maxMiss, "min_miss":minMiss, "expand":expRatio, "shrink":shrinkRatio})
+        data = {
+                        "success": "true",
+                        "mode": [mode],
+                        "numNodes": [numNodes],
+                        "cacheSize": [int(capacity/(1024*1024))],
+                        "policy": [policy]
+                    }
+        response = webapp.response_class(
+                        response=json.dumps(data),
+                        status=500,
+                        mimetype='application/json'
+                    )
+        return response
+    except Exception as e:
+        data = {
+            "success" : "false",
+            "ErrorMessage" : "lack parameters"
+        }
+        response = webapp.response_class(
+            response=json.dumps(data),
+            status=500,
+            mimetype='application/json'
+        )
+        return response
