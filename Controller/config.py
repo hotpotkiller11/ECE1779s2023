8d95e02a95697ff36700df75528870ddf14bc0ef
Winter Wang <52688110+Liquidwork@users.noreply.github.com>
2023-03-11 11:57:18 -0500

    update. to new rds

--- Controller/config.py
+++ Controller/config.py
@@ -5,7 +5,7 @@
 
 db_config = {'user': 'admin', #ur db
              'password': '12345678',
-             'host': 'ece1779sqldb.cr3xgzhrvhr7.us-east-1.rds.amazonaws.com',
+             'host': 'database-1.cxlstjvm3g8q.us-east-1.rds.amazonaws.com',
              'database': 'ece1779'}
 
 IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
